27cf99b0852fc33207a66289dfc78314a02205f2
Hannah Collins <hannahcollins53@gmail.com>
2023-05-31 12:01:10 -0700

    changed fetch calls

--- pathsapi/views/auth.py
+++ pathsapi/views/auth.py
@@ -16,26 +16,29 @@ def login_user(request):
     Method arguments:
       request -- The full HTTP request object
     '''
-    username = request.data['username']
-    password = request.data['password']
+    try:
+        username = request.data['username']
+        password = request.data['password']
 
-    # Use the built-in authenticate method to verify
-    # authenticate returns the user object or None if no user is found
-    authenticated_user = authenticate(username=username, password=password)
-    # If authentication was successful, respond with their token
-    if authenticated_user is not None:
-        token = Token.objects.get(user=authenticated_user)
-        user = User.objects.get(first_name=authenticated_user)
-        data = {
-            'valid': True,
-            'token': token.key,
-            'userId': user.id
-        }
-        return Response(data, status=status.HTTP_202_ACCEPTED)
-    else:
-        # Bad login details were provided. So we can't log the user in.
-        data = { 'valid': False }
-        return Response(data, status=status.HTTP_204_NO_CONTENT)
+        # Use the built-in authenticate method to verify
+        # authenticate returns the user object or None if no user is found
+        authenticated_user = authenticate(username=username, password=password)
+        # If authentication was successful, respond with their token
+        if authenticated_user is not None:
+            token = Token.objects.get(user=authenticated_user)
+            user = User.objects.get(first_name=authenticated_user)
+            data = {
+                'valid': True,
+                'token': token.key,
+                'userId': user.id
+            }
+            return Response(data, status=status.HTTP_202_ACCEPTED)
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            data = { 'valid': False }
+            return Response(data, status=status.HTTP_204_NO_CONTENT)
+    except:
+        return Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
