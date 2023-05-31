5a6ca7df2a5d12516ea6bd14a76238fb5dc15ddd
Hannah Collins <hannahcollins53@gmail.com>
2023-05-31 10:02:21 -0700

    changed fetch calls

--- beatenpath/settings.py
+++ beatenpath/settings.py
@@ -58,11 +58,9 @@ REST_FRAMEWORK = {
 }
 
 # THIS IS NEW
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
+CORS_ORIGIN_WHITELIST = [
     "https://beatenpath-app-pvyzi.ondigitalocean.app"
-)
+]
 
 # UPDATE THIS
 MIDDLEWARE = [
